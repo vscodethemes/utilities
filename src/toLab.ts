c867c491d42ccef51142418a3f12a00755cc1792
Jordan <github@jschr.io>
2021-12-31 18:57:48 -0500

    Round lab value to nearest integer

--- src/toLab.ts
+++ src/toLab.ts
@@ -2,5 +2,5 @@ import color from 'color';
 
 export default function toLab(value: string): [number, number, number] {
   const [l, a, b] = color(value).lab().array();
-  return [l, a, b];
+  return [Math.round(l), Math.round(a), Math.round(b)];
 }
