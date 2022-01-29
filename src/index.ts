ecdaceac0ea4e4c341f998939a1f717fe8a07268
Jordan <github@jschr.io>
2022-01-29 18:58:41 -0500

    Remove createKVData

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as assert } from './assert';
-export { default as createKVData } from './createKVData';
 export { default as env } from './env';
 export * as marketplace from './marketplace';
 export { default as toLab } from './toLab';
